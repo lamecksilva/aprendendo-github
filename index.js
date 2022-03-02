9239aee7ebe8cc6b032a35869e6e5e28d3c99f56
Lameck Sandro <lameck2@hotmail.com>
2022-03-02 15:46:56 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Faça um programa que receba a massa de um objeto = 10kg com aceleração de 10 m/s e calcule a força
+// Formula da força = Massa x aceleração
+
+const aceleracao = 10;
+var massa = 10;
+const forca = aceleracao * massa;
+
+console.log(`Massa: ${massa}Kg`);
+console.log(`Aceleração: ${aceleracao}m/s`);
+console.log(`Força: (${massa}Kg * ${aceleracao}m/s) = ${forca}N`);
